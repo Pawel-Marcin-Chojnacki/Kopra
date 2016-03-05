9b8404b6226b29f30507fde792683889b96de703
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-05 17:03:00 +0100

    Added ConnectionManager to check internet connection status.

---
+++ Kopra1/Common/ConnectionManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Kopra1.Common
+{
+    public class ConnectionManager
+    {
+        public bool CheckInternetConnection()
+        {
+            var isConnected = NetworkInterface.GetIsNetworkAvailable();
+            if (isConnected)
+            {
+                ConnectionProfile internetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
+                NetworkConnectivityLevel connection = internetConnectionProfile.GetNetworkConnectivityLevel();
+                if (connection != NetworkConnectivityLevel.None && connection != NetworkConnectivityLevel.LocalAccess) return true;
+            }
+            OnInternetConnectionFail?.Invoke();
+            return false;
+        }
+
+        public delegate void InternetConnectionFail();
+
+        public event InternetConnectionFail OnInternetConnectionFail;
+
+    }
+}
