4ae617b0eef0ecc74fc1b321498237a46735afdd
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-05 17:02:30 +0100

    Added class to show flyouts about internet status tu user.

---
+++ Kopra1/Common/Information.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Kopra1.Common
+{
+    class Information
+    {
+        public static void ShowNoConnectionInfo(object sender, string statement)
+        {
+            var flyout = new Flyout();
+            var grid = new Grid();
+            grid.Children.Add(new TextBlock()
+            {
+                Text = statement,
+                Foreground = new SolidColorBrush(Colors.White),
+                FontSize = 20,
+                HorizontalAlignment = HorizontalAlignment.Center
+            });
+            flyout.Content = grid;
+            flyout.ShowAt(sender as FrameworkElement);
+        }
+    }
+}
