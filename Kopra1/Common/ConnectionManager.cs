50110664a2b87607bc35a034dccc90997030b1db
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-06 09:08:11 +0100

    Fixed namespace to mach project convention.

--- Kopra1/Common/ConnectionManager.cs
+++ Kopra1/Common/ConnectionManager.cs
@@ -11,7 +11,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
-namespace Kopra1.Common
+namespace Kopra.Common
 {
     public class ConnectionManager
     {
