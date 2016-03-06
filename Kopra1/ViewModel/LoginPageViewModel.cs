374a6e8d9a603ac80ebac81ed296fee1144d1b80
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-06 09:10:38 +0100

    Added ViewModel for login page.

---
+++ Kopra1/ViewModel/LoginPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Kopra1.Annotations;
+
+namespace Kopra.ViewModel
+{
+    class LoginPageViewModel : INotifyPropertyChanged
+    {
+
+        public void FillLoginForm(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        private string _email;
+    
+        public string Email {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                NotifyPropertyChanged(nameof(Email));
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyPropertyChanged(nameof(Password));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
