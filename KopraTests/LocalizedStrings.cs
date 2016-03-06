5e647707786d5abb6cc132374c7a64ad55e582c3
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-06 13:05:01 +0100

    Test project init.

---
+++ KopraTests/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using KopraTests.Resources;
+
+namespace KopraTests
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ KopraTests/LoginTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace KopraTests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void ValidateEmailTest()
+        {
+
+        }
+    }
+}
