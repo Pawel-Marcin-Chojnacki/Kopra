b79d2ba2771652d1862bc546440b86ce6e869771
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-06 11:42:25 +0100

    Refactor SearchAuction method. Less dependency on string literals.

--- Kopra1/Model/RequestGenerator.cs
+++ Kopra1/Model/RequestGenerator.cs
@@ -10,10 +10,18 @@ namespace Kopra
     {
         private const string BaseAddress = "https://kokos.pl/webapi/";
         private const string DataType = "type=json";
+        private const string Search = "search?";
+        private const string RecentAuctions = "get-recent-auctions?";
+        private const string Records = "records=";
 
-        public Uri SearchAuction(Dictionary<string, string> search)
+        /// <summary>
+        /// Composes new query for searching. 
+        /// </summary>
+        /// <param name="search">Dictionary with parameters for query.</param>
+        /// <returns>URI with API request.</returns>
+        public Uri ComposeSearchAuctionQuery(Dictionary<string, string> search)
         {
-            string requestAddress = BaseAddress + "search?";
+            string requestAddress = BaseAddress + Search;
             foreach (var item in search)
             {
                 requestAddress += item.Key + "=" + item.Value + "&";
@@ -23,14 +31,17 @@ namespace Kopra
             return result;
         }
 
+        /// <summary>
+        /// Composes request to get 3 most recent auctions.
+        /// </summary>
+        /// <returns>API query to get most recent auctions.</returns>
         public Uri MostRecentAuctions()
         {
-            string requestAddress = BaseAddress + "get-recent-auctions?";
+            var requestAddress = BaseAddress + RecentAuctions;
             var sm = new SettingsManager();
             requestAddress += "key=" + sm.KokosWebApiKey + "&";
             requestAddress += DataType + "&";
-            requestAddress += "records=3";
-            
+            requestAddress += Records + "3";
             var result = new Uri(requestAddress);
             return result;
         }
