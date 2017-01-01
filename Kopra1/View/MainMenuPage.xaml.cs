2b4f826b84c346d8f4c0ea4bab5bc039b03ca5bd
Paweł Chojnacki <pawel.marcin.chojnacki@gmail.com>
2017-01-01 14:32:52 +0100

    Fix: Going to auction details from homepage no longer causes break

--- Kopra1/View/MainMenuPage.xaml.cs
+++ Kopra1/View/MainMenuPage.xaml.cs
@@ -11,102 +11,102 @@ using Kopra.ViewModel;
 
 namespace Kopra
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainMenuPage : Page
-    {
-        private NavigationHelper navigationHelper;
-        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenuPage : Page
+	{
+		private NavigationHelper navigationHelper;
+		private ObservableDictionary defaultViewModel = new ObservableDictionary();
 
-        public MainMenuPage()
-        {
-            InitializeComponent();
+		public MainMenuPage()
+		{
+			InitializeComponent();
 
-            navigationHelper = new NavigationHelper(this);
-            navigationHelper.LoadState += NavigationHelper_LoadState;
-            navigationHelper.SaveState += NavigationHelper_SaveState;
-        }
+			navigationHelper = new NavigationHelper(this);
+			navigationHelper.LoadState += NavigationHelper_LoadState;
+			navigationHelper.SaveState += NavigationHelper_SaveState;
+		}
 
-        /// <summary>
-        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
-        /// </summary>
-        public NavigationHelper NavigationHelper
-        {
-            get { return navigationHelper; }
-        }
+		/// <summary>
+		/// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+		/// </summary>
+		public NavigationHelper NavigationHelper
+		{
+			get { return navigationHelper; }
+		}
 
-        /// <summary>
-        /// Gets the view model for this <see cref="Page"/>.
-        /// This can be changed to a strongly typed view model.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return defaultViewModel; }
-        }
+		/// <summary>
+		/// Gets the view model for this <see cref="Page"/>.
+		/// This can be changed to a strongly typed view model.
+		/// </summary>
+		public ObservableDictionary DefaultViewModel
+		{
+			get { return defaultViewModel; }
+		}
 
-        /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="sender">
-        /// The source of the event; typically <see cref="NavigationHelper"/>
-        /// </param>
-        /// <param name="e">Event data that provides both the navigation parameter passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
-        /// a dictionary of state preserved by this page during an earlier
-        /// session.  The state will be null the first time a page is visited.</param>
-        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
-        {
-            UserCredentials.SetUserName(userNameTitle);
-            StatusBarManager.SetStyle();
-            Frame.BackStack.Clear();
-        }
+		/// <summary>
+		/// Populates the page with content passed during navigation.  Any saved state is also
+		/// provided when recreating a page from a prior session.
+		/// </summary>
+		/// <param name="sender">
+		/// The source of the event; typically <see cref="NavigationHelper"/>
+		/// </param>
+		/// <param name="e">Event data that provides both the navigation parameter passed to
+		/// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+		/// a dictionary of state preserved by this page during an earlier
+		/// session.  The state will be null the first time a page is visited.</param>
+		private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+		{
+			UserCredentials.SetUserName(userNameTitle);
+			StatusBarManager.SetStyle();
+			Frame.BackStack.Clear();
+		}
 
-        /// <summary>
-        /// Preserves state associated with this page in case the application is suspended or the
-        /// page is discarded from the navigation cache.  Values must conform to the serialization
-        /// requirements of <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
-        /// <param name="e">Event data that provides an empty dictionary to be populated with
-        /// serializable state.</param>
-        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
-        {
-        }
+		/// <summary>
+		/// Preserves state associated with this page in case the application is suspended or the
+		/// page is discarded from the navigation cache.  Values must conform to the serialization
+		/// requirements of <see cref="SuspensionManager.SessionState"/>.
+		/// </summary>
+		/// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+		/// <param name="e">Event data that provides an empty dictionary to be populated with
+		/// serializable state.</param>
+		private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+		{
+		}
 
-        #region NavigationHelper registration
+		#region NavigationHelper registration
 
-        /// <summary>
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// <para>
-        /// Page specific logic should be placed in event handlers for the
-        /// <see cref="NavigationHelper.LoadState"/>
-        /// and <see cref="NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method
-        /// in addition to page state preserved during an earlier session.
-        /// </para>
-        /// </summary>
-        /// <param name="e">Provides data for navigation methods and event
-        /// handlers that cannot cancel the navigation request.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            navigationHelper.OnNavigatedTo(e);
-            //Frame.BackStack.Clear();
-        }
+		/// <summary>
+		/// The methods provided in this section are simply used to allow
+		/// NavigationHelper to respond to the page's navigation methods.
+		/// <para>
+		/// Page specific logic should be placed in event handlers for the
+		/// <see cref="NavigationHelper.LoadState"/>
+		/// and <see cref="NavigationHelper.SaveState"/>.
+		/// The navigation parameter is available in the LoadState method
+		/// in addition to page state preserved during an earlier session.
+		/// </para>
+		/// </summary>
+		/// <param name="e">Provides data for navigation methods and event
+		/// handlers that cannot cancel the navigation request.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			navigationHelper.OnNavigatedTo(e);
+			//Frame.BackStack.Clear();
+		}
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            navigationHelper.OnNavigatedFrom(e);
-        }
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+			navigationHelper.OnNavigatedFrom(e);
+		}
 
-        #endregion
+		#endregion
 
-        private void addFilterButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(AddFilterPage));
-        }
+		private void addFilterButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(AddFilterPage));
+		}
 
 
 		private void ShowFilters(object sender, RoutedEventArgs e)
@@ -115,24 +115,24 @@ namespace Kopra
 		}
 
 		private void searchAuctionButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(View.SearchPage));
-        }
+		{
+			Frame.Navigate(typeof(View.SearchPage));
+		}
 
-        private void settingsButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(SettingsPage));
-        }
+		private void settingsButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(SettingsPage));
+		}
 
-        /// <summary>
-        /// Go to page summarizing investor report.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void inverstorReportButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(InvestorReportPage));
-        }
+		/// <summary>
+		/// Go to page summarizing investor report.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void inverstorReportButton_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(InvestorReportPage));
+		}
 
 		/// <summary>
 		/// Load auction details view.
@@ -142,7 +142,9 @@ namespace Kopra
 		private void LoadAuction(object sender, ItemClickEventArgs e)
 		{
 			var vm = (MainMenuViewModel) this.DataContext;
-			Frame.Navigate(typeof(AuctionBasicDataPage), e.ClickedItem);
+			Auction selectedAuction = (Auction)e.ClickedItem;
+			var id = selectedAuction.id;
+			Frame.Navigate(typeof(AuctionBasicDataPage), id);
 		}
 	}
 }
